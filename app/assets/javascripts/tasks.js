e6a7c7258758a55bc9dd0310219c82354b995321
MH Raihan <me@mhraihan.com>
2016-04-11 00:20:05 +0600

    added jquery-ui gem, modify task for ajax request

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,9 +4,8 @@
 $(document).on('ready page:load', function(event) {
   $(".task_done").on("change",function () {
     var self = $(this);
-    console.log("what's going on there");
     $.post("/tasks/"+self.data("id")+"/toggle",{"_method":"patch"},function () {
-      console.log("done");
+      self.parent().effect("highlight");
     });
   });
 });
