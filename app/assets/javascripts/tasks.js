077cc6f90a82d68e16c644f418f1067884de9c71
MH Raihan <me@mhraihan.com>
2016-04-10 22:43:11 +0600

    modify tasks.js,added trubolink page load event and seems it's work, really?

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,12 @@
-(function () {
+/* Adding page load event solve my probelm
+  Is there any better way to this ?
+*/
+$(document).on('ready page:load', function(event) {
   $(".task_done").on("change",function () {
     var self = $(this);
     console.log("what's going on there");
-    $.post("/tasks"+self.data("id")+"/toggle",{"_method":"patch"},function () {
+    $.post("/tasks/"+self.data("id")+"/toggle",{"_method":"patch"},function () {
       console.log("done");
     });
   });
-})();
+});
